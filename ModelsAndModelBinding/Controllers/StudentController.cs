c63a3994a6abcaf19304f0ccd5b8bf6ef716c0ec
Dennis Kafton <boom1axe@att.net>
2019-07-11 11:01:25 -0700

    Register page and Data Search start

---
+++ ModelsAndModelBinding/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsAndModelBinding.Models;
+
+namespace ModelsAndModelBinding.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // when data is posted to /student/Register
+        // this action will execute
+        [HttpPost]
+        public IActionResult Register(IFormCollection data)
+        {
+            // Validate input (lot of work!)
+            // Server-side and client-side
+
+            Student stu = new Student()
+            {
+                FullName = data["full-name"],
+                ProgramOfChoice = data["program"],
+                DateOfBirth = Convert.ToDateTime(data["dob"]),
+                homeAddress = data["address"]
+            };
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegisterMB()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterMB(Student s)
+        {
+            return View();
+        }
+    }
+}
---
+++ ModelsAndModelBinding/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+
+namespace ModelsAndModelBinding.Models
+{
+    /// <summary>
+    /// Represents an individual student
+    /// </summary>
+    public class Student
+    {
+        
+        public int StudentID { get; set; }
+        /// <summary>
+        /// The legal First then last name
+        /// ex. James Bond
+        /// </summary>
+        public String FullName { get; set; }
+        public String ProgramOfChoice { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String homeAddress { get; set; }
+    }
+}
